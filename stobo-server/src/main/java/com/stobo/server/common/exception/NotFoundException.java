c514456da620632d0e62eaa9a68a207706137396
Marcos Brito <marcos.brito0507@gmail.com>
2025-06-16 23:19:11 -0300

    fix: add quotes around ID in NotFoundException

--- stobo-server/src/main/java/com/stobo/server/common/exception/NotFoundException.java
+++ stobo-server/src/main/java/com/stobo/server/common/exception/NotFoundException.java
@@ -2,6 +2,6 @@ package com.stobo.server.common.exception;
 
 public class NotFoundException extends RuntimeException {
   public NotFoundException(String entity, String id) {
-    super(String.format("%s with id %s not found", entity, id));
+    super(String.format("%s with id \"%s\" not found", entity, id));
   }
 }
